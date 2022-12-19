726f8138bd8948d45091284e9fc9e3bfd14849c2
dv970406 <dv970406@gmail.com>
2022-12-19 18:44:34 +0900

    nice structure of test

--- unit-test/src/mock/product/test/product_service_no_di.test.js
+++ unit-test/src/mock/product/test/product_service_no_di.test.js
@@ -1,11 +1,13 @@
-const ProductService = require('../product_service_no_di.js');
-const ProductClient = require('../product_client.js');
-jest.mock('../product_client');
+const ProductService = require("../product_service_no_di.js");
+const ProductClient = require("../product_client.js");
+const StubProductClient = require("./stub_product_client.js");
 
-describe('ProductService', () => {
+jest.mock("../product_client");
+
+describe("ProductService", () => {
   const fetchItems = jest.fn(async () => [
-    { item: '🥛', available: true },
-    { item: '🍌', available: false },
+    { item: "🥛", available: true },
+    { item: "🍌", available: false },
   ]);
   ProductClient.mockImplementation(() => {
     return {
@@ -20,13 +22,19 @@ describe('ProductService', () => {
     ProductClient.mockClear();
   });
 
-  it('should filter out only available items', async () => {
+  it("should filter out only available items", async () => {
+    // Given
+    const productService = new ProductService(new StubProductClient());
+
+    // When
     const items = await productService.fetchAvailableItems();
+
+    // Then
     expect(items.length).toBe(1);
-    expect(items).toEqual([{ item: '🥛', available: true }]);
+    expect(items).toEqual([{ item: "🥛", available: true }]);
   });
 
-  it('test', async () => {
+  it("test", async () => {
     const items = await productService.fetchAvailableItems();
     expect(fetchItems).toHaveBeenCalledTimes(1);
   });
