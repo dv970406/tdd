515b37b258a00cc4d1e6aaefcd755773fa363db8
dv970406 <dv970406@gmail.com>
2022-12-23 11:25:13 +0900

    YoutubeProvider 설명

--- youtube-final/src/context/YoutubeProvider.jsx
+++ youtube-final/src/context/YoutubeProvider.jsx
@@ -1,9 +1,13 @@
 import Youtube from "../api/youtube";
 import YoutubeClient from "../api/youtubeClient";
+import { YoutubeApiContext } from "./YoutubeApiContext";
 
 const client = new YoutubeClient();
 const youtube = new Youtube(client);
 
+// 테스트가 아닌 Application에서 사용할 Provider
+// 테스트에서는 이 Provider가 아닌 YoutubeApiContext를 이용하게 되므로
+// YoutubeClient를 테스트에서 사용하지 않게되어 axios를 사용하지 않는다.
 export function YoutubeApiProvider({ children }) {
   return (
     <YoutubeApiContext.Provider value={{ youtube }}>
