9e70502d4ac079dff4973e45ba8f895f6bea866e
dv970406 <dv970406@gmail.com>
2022-12-26 13:30:41 +0900

    E2E 테스트 마무리

--- youtube-final/src/context/YoutubeApiContext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const YoutubeApiContext = createContext();
-
-export function useYoutubeApi() {
-  return useContext(YoutubeApiContext);
-}
