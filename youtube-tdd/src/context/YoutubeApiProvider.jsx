5d36ccb1cee9a1ff79e9e249f1f5e8f7a9100ea4
최성준 <dv970406@gmail.com>
2022-12-31 16:44:35 +0900

    주석 추가

--- youtube-tdd/src/context/YoutubeApiProvider.jsx
+++ youtube-tdd/src/context/YoutubeApiProvider.jsx
@@ -8,6 +8,7 @@ const youtube = new Youtube(client);
 // 테스트가 아닌 Application에서 사용할 Provider
 // 테스트에서는 이 Provider가 아닌 YoutubeApiContext를 이용하게 되므로
 // YoutubeClient를 테스트에서 사용하지 않게되어 axios를 사용하지 않는다.
+// 테스트에서는 네트워크 통신을 하는 것이 아니라 Mock데이터를 쓸 것이기 때문이다.
 export function YoutubeApiProvider({ children }) {
   return (
     <YoutubeApiContext.Provider value={{ youtube }}>
